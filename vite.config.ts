c2b8c7a376d29a37ba79eb28f50a67d628d00cbe
Momen Sherif <momensherif.2019@gmail.com>
2022-05-15 18:27:48 +0200

    chore: add linters, husky and lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+});
