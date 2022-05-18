bb30a6d2c129e8d41dbbeed57ee1e9d412b564c3
Momen Sherif <momensherif.2019@gmail.com>
2022-05-18 20:58:14 +0200

    feat: add basic PWA setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/700.css';
+import { registerSW } from 'virtual:pwa-register';
 
 import App from './App';
 
@@ -26,3 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </ChakraProvider>
   </React.StrictMode>,
 );
+
+registerSW({
+  onOfflineReady() {},
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,47 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'BulQR',
+        short_name: 'BulQR',
+        background_color: '#FF0000',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: 'icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     target: ['es2020'],
   },
